8da537c60b8c180d501d9fa2a35d60cbdca521ad
jerabaul29 <jean.rblt@gmail.com>
2019-04-23 17:22:38 +0200

    mostly working, but need to be able to kill also

--- python_solution/python_keep_alive.py
+++ python_solution/python_keep_alive.py
@@ -16,6 +16,37 @@ def run_command(exe, pooling_frequency=1.0):
         if retcode is not None:
             break
 
+
+def launch_and_keep_alive(command, frequency_check=1.0, time_to_start_s=60.0):
+
+    process_active = False
+
+    while(True):
+
+        # if no process active, restart one
+        if not process_active:
+            yielder = run_command(command, pooling_frequency=2.0 * frequency_check)
+            time.sleep(time_to_start_s)
+            process_active = True
+
+        time.sleep(1.0 / frequency_check)
+
+        # at this stage, the process is active; need to check if some output has
+        # come out; if not, kill and restart
+
+        try:
+            # try to get one more; if exhausted of dead, it will except
+            print(next(yielder))
+
+            # if not exhausted or dead, empty
+            for crrt_elem in yielder:
+                print(crrt_elem)
+
+        except:
+            print("dead or stalled processed!")
+            process_active = False
+
+
 #  def run_command(command):
 #      p = subprocess.Popen(command,
 #                           stdout=subprocess.PIPE,
@@ -34,5 +65,7 @@ def run_command(exe, pooling_frequency=1.0):
 #      return rc
 
 
-for line in run_command('python3 python_ping.py'):
-    print(line)
+#  for line in run_command('python3 python_ping.py'):
+#      print(line)
+
+launch_and_keep_alive('python3 python_ping.py', time_to_start_s=0.5)
